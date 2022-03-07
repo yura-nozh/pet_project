a0b01fb9006baf56e4a1d8a07083d928af943cc8
yura-nozh <arctg07@gmail.com>
2022-03-07 16:29:22 +0300

    add bean converter

---
+++ src/main/java/com/example/yuriy_ivanov/configurations/Config.java
@@ -0,0 +1,21 @@
+package com.example.yuriy_ivanov.configurations;
+
+import com.example.yuriy_ivanov.services.Converter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public Converter converter(ObjectMapper objectMapper) {
+       return new Converter(objectMapper);
+    }
+
+}
---
+++ src/main/java/com/example/yuriy_ivanov/services/Converter.java
@@ -0,0 +1,11 @@
+package com.example.yuriy_ivanov.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Converter {
+
+    private ObjectMapper objectMapper;
+
+}
